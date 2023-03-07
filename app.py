32411977d20444c67356aa72d9d78f7205a0e0da
Christopher Tate <computate@computate.org>
2023-03-07 10:07:14 -0700

    Adding error handling in messages

--- app.py
+++ app.py
@@ -48,7 +48,17 @@ def test_topic_handler(msg):
                 , iters_per_par=iters_per_par
                 , print_mode=False)
     except Exception as e:
-        print("an error occured processing a message on %s topic: %s\n%s" % (kafka_topic_sumo_run, str(e), msg))
+        ex_type, ex_value, ex_traceback = sys.exc_info()
+        # Extract unformatter stack traces as tuples
+        trace_back = traceback.extract_tb(ex_traceback)
+    
+        # Format stacktrace
+        stack_trace = list()
+    
+        for trace in trace_back:
+            stack_trace.append("File : %s , Line : %d, Func.Name : %s, Message : %s" % (trace[0], trace[1], trace[2], trace[3]))
+    
+        print("%s occured processing a message on %s topic: %s\n%s" % (ex_type.__name__, kafka_topic_sumo_run, ex_value, stack_trace))
 
 def listen_kill_server():
     signal.signal(signal.SIGTERM, bus.interrupted_process)
